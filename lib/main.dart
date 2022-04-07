77145aa8e2a1cdda4f7be66505d47c84b07a2553
CelioHauck <celiohauck@gmail.com>
2022-04-07 16:27:14 -0300

    Testando e estudando MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -112,28 +112,48 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: const Text('Personal Expenses'),
+      actions: [
+        IconButton(
+          onPressed: () => _startAddNewTransaction(context),
+          icon: const Icon(Icons.add),
+        )
+      ],
+    );
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Personal Expenses'),
-        actions: [
-          IconButton(
-            onPressed: () => _startAddNewTransaction(context),
-            icon: const Icon(Icons.add),
-          )
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Chart(
-              transactions: _recentTransactions,
+            SizedBox(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.4,
+              child: Chart(
+                transactions: _recentTransactions,
+              ),
             ),
-            TransactionList(
-              userTransactions: _userTransactions,
-              onRemove: _removeTransaction,
+            SizedBox(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.6,
+              child: TransactionList(
+                userTransactions: _userTransactions,
+                onRemove: _removeTransaction,
+              ),
             ),
+            // Chart(
+            //   transactions: _recentTransactions,
+            // ),
+            // TransactionList(
+            //   userTransactions: _userTransactions,
+            //   onRemove: _removeTransaction,
+            // ),
           ],
         ),
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,105 +15,101 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      child: _userTransactions.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  'Não encontramos uma transação',
-                  style: Theme.of(context).textTheme.headline6,
+    return _userTransactions.isEmpty
+        ? Column(
+            children: [
+              Text(
+                'Não encontramos uma transação',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(3),
-                        child: FittedBox(
-                          child: Text(
-                            _userTransactions[index].getAmountWithCurrency(),
-                          ),
+              )
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(3),
+                      child: FittedBox(
+                        child: Text(
+                          _userTransactions[index].getAmountWithCurrency(),
                         ),
                       ),
                     ),
-                    title: Text(
-                      _userTransactions[index].getTitle(),
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      _userTransactions[index].getDateFormat(),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => _onRemove(
-                        _userTransactions[index].hashCode,
-                      ),
+                  ),
+                  title: Text(
+                    _userTransactions[index].getTitle(),
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    _userTransactions[index].getDateFormat(),
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => _onRemove(
+                      _userTransactions[index].hashCode,
                     ),
                   ),
-                );
-                // return Card(
-                //   child: Row(
-                //     children: [
-                //       Container(
-                //         margin: const EdgeInsets.symmetric(
-                //           vertical: 10,
-                //           horizontal: 15,
-                //         ),
-                //         decoration: BoxDecoration(
-                //           border: Border.all(
-                //             color: Theme.of(context).colorScheme.primary,
-                //             width: 2,
-                //           ),
-                //         ),
-                //         padding: const EdgeInsets.all(10),
-                //         child: Text(
-                //           _userTransactions[index].getAmountWithCurrency(),
-                //           style: TextStyle(
-                //             fontSize: 18,
-                //             color: Theme.of(context).colorScheme.primary,
-                //             fontWeight: FontWeight.bold,
-                //           ),
-                //         ),
-                //       ),
-                //       Column(
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: [
-                //           Text(_userTransactions[index].getTitle(),
-                //               style: Theme.of(context).textTheme.headline6),
-                //           Text(
-                //             _userTransactions[index].getDateFormat(),
-                //             style: const TextStyle(
-                //               color: Colors.grey,
-                //               fontSize: 14,
-                //             ),
-                //           ),
-                //         ],
-                //       ),
-                //     ],
-                //   ),
-                // );
-              },
-              itemCount: _userTransactions.length,
-            ),
-    );
+                ),
+              );
+              // return Card(
+              //   child: Row(
+              //     children: [
+              //       Container(
+              //         margin: const EdgeInsets.symmetric(
+              //           vertical: 10,
+              //           horizontal: 15,
+              //         ),
+              //         decoration: BoxDecoration(
+              //           border: Border.all(
+              //             color: Theme.of(context).colorScheme.primary,
+              //             width: 2,
+              //           ),
+              //         ),
+              //         padding: const EdgeInsets.all(10),
+              //         child: Text(
+              //           _userTransactions[index].getAmountWithCurrency(),
+              //           style: TextStyle(
+              //             fontSize: 18,
+              //             color: Theme.of(context).colorScheme.primary,
+              //             fontWeight: FontWeight.bold,
+              //           ),
+              //         ),
+              //       ),
+              //       Column(
+              //         crossAxisAlignment: CrossAxisAlignment.start,
+              //         children: [
+              //           Text(_userTransactions[index].getTitle(),
+              //               style: Theme.of(context).textTheme.headline6),
+              //           Text(
+              //             _userTransactions[index].getDateFormat(),
+              //             style: const TextStyle(
+              //               color: Colors.grey,
+              //               fontSize: 14,
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //     ],
+              //   ),
+              // );
+            },
+            itemCount: _userTransactions.length,
+          );
   }
 }
